eae05e6583a22bc2c0588a59f40f6305a9bbde4c
elkik <elkik@LAPTOP-FDAOLACM>
2022-11-15 15:27:14 -0600

    Ejercicio uno jiji

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -7,6 +7,10 @@ public class Mavenproject1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
            System.out.println("Hello World!");
+            System.out.println("Hello World!");
+           System.out.println("Hello World!");
+            System.out.println("Hello World!");
+           System.out.println("Hello World!");
     }
 }
 
